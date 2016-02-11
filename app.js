56afa7026f419ef81c35cf64e3fc605dfa4262d5
Micah Schow <micah.schow@gmail.com>
2016-02-10 23:04:33 -0700

    Completed part 2

--- app.js
+++ app.js
@@ -1,8 +1,44 @@
 var reps = require('./lib/reps');
+var express = require('express');
+var app = express ();
 
-console.log(reps);
-
-reps.allByZip('84043', function(err, results) {
-  if (err) return console.log(err);
-  console.log(results);
+app.get('/all/by-zip/:zip', function(req, res, next) {
+  reps.allByZip(req.params.zip, function(err, people){
+    if (err){ return next(err); }
+    res.json(people);
+  });
 });
+
+app.get('/reps/by-name/:name', function(req, res, next) {
+  reps.repsByName(req.params.name, function(err, people){
+    if (err){ return next(err); }
+    res.json(people);
+  });
+});
+
+app.get('/reps/by-state/:state', function(req, res, next) {
+  reps.repsByState(req.params.state, function(err, people){
+    if (err){ return next(err); }
+    res.json(people);
+  });
+});
+
+app.get('/sens/by-name/:name', function(req, res, next) {
+  reps.sensByName(req.params.name, function(err, people){
+    if (err){ return next(err); }
+    res.json(people);
+  });
+});
+
+app.get('/sens/by-state/:state', function(req, res, next) {
+  reps.sensByState(req.params.state, function(err, people){
+    if (err){ return next(err); }
+    res.json(people);
+  });
+});
+
+app.listen(3000,function() {
+  console.log('Server listening on port 3000');
+});
+
+module.exports = app;
